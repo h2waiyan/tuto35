df135f76ca04f86a20e1ac2930500f07d9f4989f
Htoo Wai Yan <95065183+h2waiyan@users.noreply.github.com>
2023-11-20 12:14:25 +0630

    external command

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const dbConfig = require("../config/db.config");
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
-  `postgres://hwy:mcWmWF404ZkN69dd5nviVnQ3jMMBIwWP@dpg-cldeodvgsrdc73fk39m0-a/tours_tu8r`,
+  `postgres://hwy:mcWmWF404ZkN69dd5nviVnQ3jMMBIwWP@dpg-cldeodvgsrdc73fk39m0-a.oregon-postgres.render.com/tours_tu8r?ssl=true`,
   { dialect: "postgres" }
 );
 
